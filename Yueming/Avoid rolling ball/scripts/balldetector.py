dc44bbb0c25d677c836157504319d3d22981e846
CathyYueming <congyueming@gmail.com>
2017-04-12 08:08:21 -0600

    have not finish, maybe use Hough Circle Transform
    something have not decide yet. because the ball is observed as a circle from any angle.

---
+++ Yueming/Avoid rolling ball/scripts/balldetector.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+
+#http://wiki.ros.org/rospy_tutorials/Tutorials/WritingImagePublisherSubscriber
+
+
+
+
+__author__ =  'Simon Haller <simon.haller at uibk.ac.at>'
+__version__=  '0.1'
+__license__ = 'BSD'
+# Python libs
+import sys, time
+
+# numpy and scipy
+import numpy as np
+from scipy.ndimage import filters
+
+# OpenCV
+import cv2
+
+# Ros libraries
+import roslib
+import rospy
+
+# Ros Messages
+from sensor_msgs.msg import CompressedImage
+# We do not use cv_bridge it does not support CompressedImage in python
+# from cv_bridge import CvBridge, CvBridgeError
+
+VERBOSE=False
+ 
+class image_feature:
+
+    def __init__(self):
+    '''Initialize ros publisher, ros subscriber'''
+    # topic where we publish
+        self.image_pub = rospy.Publisher("/output/image_raw/compressed",CompressedImage)
+        # self.bridge = CvBridge()
+ 
+       # subscribed Topic
+        self.subscriber = rospy.Subscriber("/camera/image/compressed",CompressedImage, self.callback,  queue_size = 1)
+        if VERBOSE :
+            print "subscribed to /camera/image/compressed"
+
+    def callback(self, ros_data):
+        '''Callback function of subscribed topic. 
+        Here images get converted and features detected'''
+        if VERBOSE :
+            print 'received image of type: "%s"' % ros_data.format
+
+        #### direct conversion to CV2 ####
+        np_arr = np.fromstring(ros_data.data, np.uint8)
+        image_np = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+        
+        #### Feature detectors using CV2 #### 
+        # "","Grid","Pyramid" + 
+        # "FAST","GFTT","HARRIS","MSER","ORB","SIFT","STAR","SURF"
+        method = "GridFAST"
+        feat_det = cv2.FeatureDetector_create(method)
+        time1 = time.time()
+
+        # convert np image to grayscale
+        featPoints = feat_det.detect(
+            cv2.cvtColor(image_np, cv2.COLOR_BGR2GRAY))
+        time2 = time.time()
+        if VERBOSE :
+            print '%s detector found: %s points in: %s sec.'%(method,
+                len(featPoints),time2-time1)
+
+        for featpoint in featPoints:
+            x,y = featpoint.pt
+            cv2.circle(image_np,(int(x),int(y)), 3, (0,0,255), -1)
+        
+        cv2.imshow('cv_img', image_np)
+        cv2.waitKey(2)
+
+        #### Create CompressedIamge ####
+        msg = CompressedImage()
+        msg.header.stamp = rospy.Time.now()
+        msg.format = "jpeg"
+        msg.data = np.array(cv2.imencode('.jpg', image_np)[1]).tostring()
+        # Publish new image
+        self.image_pub.publish(msg)
+        
+        #self.subscriber.unregister()
+
+def main(args):
+    '''Initializes and cleanup ros node'''
+    ic = image_feature()
+    rospy.init_node('image_feature', anonymous=True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print "Shutting down ROS Image feature detector module"
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+'''
+import cv2
+import numpy as np
+
+img = cv2.imread('opencv_logo.png',0)
+img = cv2.medianBlur(img,5)
+cimg = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,
+                            param1=50,param2=30,minRadius=0,maxRadius=0)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+
+cv2.imshow('detected circles',cimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+'''
